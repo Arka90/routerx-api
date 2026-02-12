56577a0d641ae3513c24cd43450255c778a0f0f3
arka90 <skarka90@gmail.com>
2026-02-13 00:58:57 +0530

    test deploy

--- src/webhook.ts
+++ src/webhook.ts
@@ -1,15 +1,19 @@
-import { exec } from "child_process";
+import { spawn } from "child_process";
 import express from "express";
 
 export function registerWebhook(app: express.Express) {
   app.post("/deploy", (req, res) => {
     console.log("GitHub webhook received");
 
-    exec("bash ~/deploy.sh", (err, stdout, stderr) => {
-      console.log(stdout);
-      console.log(stderr);
+    // respond FIRST
+    res.status(200).send("OK");
+
+    // then deploy in background
+    const child = spawn("bash", ["/root/deploy.sh"], {
+      detached: true,
+      stdio: "ignore"
     });
 
-    res.send("Deploy started");
+    child.unref();
   });
 }
